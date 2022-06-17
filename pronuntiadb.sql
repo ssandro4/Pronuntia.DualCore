4e2f1028f16d479a3c8dcec69aecddd2c9d38557
ssandro4 <l.sergio.1984@gmail.com>
2022-06-17 16:56:52 +0200

    update db

--- pronuntiadb.sql
+++ pronuntiadb.sql
@@ -3,12 +3,15 @@ create database if not exists pronuntiadb;
 use pronuntiadb;
 
 create table Logopedista(
-idLogopedista int primary key,
+idLogopedista int auto_increment primary key,
 nome varchar(24),
 cognome varchar(24),
 email varchar(24),
-password varchar(16)
+password varchar(16),
+authKey varchar(12) ,
+accessToken varchar(9)
 );
+
 create table Caregiver(
 idCaregiver int auto_increment primary key,
 nome varchar(24),
@@ -20,8 +23,8 @@ accessToken varchar(9)
 
 );
 
-create table Paziente(
-idPaziente int primary key,
+create table paziente(
+idPaziente int auto_increment primary key,
 nome varchar(24),
 cognome varchar(24),
 caregiver int,
@@ -30,22 +33,65 @@ FOREIGN KEY (caregiver) references Caregiver(idCaregiver),
 FOREIGN KEY (logopedista) references Logopedista(idLogopedista));
 
 create table Parola(
-parola varchar(24) PRIMARY KEY,
+idParola varchar(24) PRIMARY KEY,
 tag varchar(52),
 pathIMG varchar(52),
 pathMP3 varchar (52));
+
+create table Esercizio(
+idEsercizio varchar(52) primary key,
+tipo enum('audio','immagine')
+);
+
+create table ComposizioneEsercizio(
+esercizio varchar(52),
+parola varchar(24),
+FOREIGN KEY (esercizio) references Esercizio(idEsercizio),
+FOREIGN KEY (parola) references Parola(idParola),
+primary key (esercizio, parola)
+);
+
+create table Sessione(
+idSessione varchar(24) primary key,
+note varchar(256)
+);
+
+create table ComposizioneSessione(
+sessione varchar(24),
+esercizio varchar(52),
+foreign key (sessione) references Sessione(idSessione),
+foreign key (esercizio) references Esercizio(idEsercizio),
+PRIMARY KEY (sessione, esercizio)
+);
+
+create table AssegnazioneSessione(
+sessione varchar(24),
+paziente int,
+esito enum('corretto', 'errato', 'non svolto') default null,
+nuovo boolean default true,
+primary key (sessione, paziente),
+foreign key (sessione) references Sessione(idSessione),
+foreign key (paziente) references Paziente(idPaziente)
+);
 
 create table CoppiaMinima(
 idCoppia varchar(52) PRIMARY KEY,
 parola1 varchar(24),
 parola2 varchar(24),
-FOREIGN KEY (parola1) references Parola(parola),
-FOREIGN KEY (parola2) references Parola(parola));
+FOREIGN KEY (parola1) references Parola(idParola),
+FOREIGN KEY (parola2) references Parola(idParola));
 
 insert into Logopedista value
-(1,"Mario","Rossi", "mar.rosso@libero.it","iLoveZumba");
+(1,"Mario","Rossi", "mar.rosso@libero.it","LogoLoco","test200key","200-token");
 
 insert into Caregiver (nome,cognome,email,password,authKey,accessToken) values
 ("Giovanni","Marroni", "giov.mar@gmail.com","Ketchup3Maionese","test100key","100-token"),
-("Simone","Verdi","simo.ver@tiscali.it","SimoneTrimone","test101key","101-token");
+("Simone","Verdi","simo.ver@tiscali.it","SimoneLimone","test101key","101-token");
 select * from caregiver;
+
+insert into Paziente value
+(1,"Pino","Pini",1,1);
+
+select * from paziente;
+select * from logopedista;
+select * from paziente, logopedista, caregiver;

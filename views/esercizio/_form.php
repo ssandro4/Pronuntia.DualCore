8aef2677ccbf358b4b35d46e4188c61348ee7596
GITprogetto <kingflushslush@gmail.com>
2022-06-20 18:47:26 +0200

    Esercizio (confilitto)

--- views/esercizio/_form.php
+++ views/esercizio/_form.php
@@ -1,13 +1,11 @@
 <?php
 
-use app\models\Caregiver;
-use app\models\ParolaSearch;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\Esercizio */
 /* @var $form yii\widgets\ActiveForm */
+
 ?>
 
 <div class="esercizio-form">
@@ -18,11 +16,15 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'parola')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'parola2')->textInput(['maxlength' => true]) ?>
+    <?=  
+        $form->field($model, 'tipo')->radioList([
+        'Audio' => 'Audio', 
+        'Immagine' => 'Immagine', 
+        'Coppia Minima' => 'Coppia Minima'
+        ]);
+        ?>
 
-    <?= $form->field($model, 'tipo')->dropDownList([ 'Audio' => 'Audio', 'Immagine' => 'Immagine', 'Coppia Minima' => 'Coppia Minima', ], ['prompt' => '']) ?>
-    
-
+    <?=  $form->field($model, 'parola2')->textInput(['maxlength' => true, 'readonly' => $model->tipo!=='Coppia Minima' ])  ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>

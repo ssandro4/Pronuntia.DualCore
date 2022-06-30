67922ec118af4ce4aa53f9568d265236f14a1656
GITprogetto <kingflushslush@gmail.com>
2022-06-30 13:02:22 +0200

    Parola gridview

--- views/parola/index.php
+++ views/parola/index.php
@@ -1,10 +1,12 @@
 <?php
 
+use app\models\Parola;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\ActionColumn;
 use yii\widgets\ListView;
-
+use yii\widgets\Pjax;
+use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\ParolaSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
@@ -20,15 +22,51 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a('Create Parola', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
+    <?php Pjax::begin(); ?>
     <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= ListView::widget([
+<!--
+    < ?= ListView::widget([
         'dataProvider' => $dataProvider,
         'itemOptions' => ['class' => 'item'],
         'itemView' => function ($model, $key, $index, $widget) {
             return Html::a(Html::encode($model->idParola), ['view', 'idParola' => $model->idParola]);
         },
     ]) ?>
+-->
 
+<?= GridView::widget([
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
+        
+        'Id Parola' =>'idParola',
+        'Tag' => 'tag',
+        
+        [
 
-</div>
+            'attribute' => 'img',
+            'format' => 'html',
+            'label' => 'Immagine',
+
+            'value' => function ($model) {
+
+                return Html::img( $model['pathIMG'],
+                    ['width' => '140px','height' => '100px' ]);
+            },
+        ],
+        
+        'File Audio' => 'pathMP3',
+
+        [
+            'class' => ActionColumn::className(),
+            'urlCreator' => function ($action, Parola $model, $key, $index, $column) {
+                return Url::toRoute([$action, 'idParola' => $model->idParola]);
+             }
+        ],
+
+    ],
+    
+]) ?>
+
+    <?php Pjax::end(); ?>

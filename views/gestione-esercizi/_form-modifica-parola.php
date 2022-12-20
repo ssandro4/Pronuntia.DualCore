01369851e7cd8190edb490440f337f57fa4f5037
ssandro4 <l.sergio.1984@gmail.com>
2022-12-20 19:04:42 +0100

    Update _form-modifica-parola.php

--- views/gestione-esercizi/_form-modifica-parola.php
+++ views/gestione-esercizi/_form-modifica-parola.php
@@ -19,7 +19,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'pathMP3')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group"> 
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Salva', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
